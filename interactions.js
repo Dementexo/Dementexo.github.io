dbedbe2580d8b5f0de68ddb89865bca92f2d9060
Dementexo <browndon962@gmail.com>
2022-06-24 22:10:36 -0500

    more text changes

--- interactions.js
+++ interactions.js
@@ -1,27 +1,26 @@
-let introTC = document.createElement("introTextContainer");
-let introText = document.createTextNode("Whats brackin");
+let introTextContainer = document.createElement('iTC');
+let introText = document.createTextNode("Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.");
 let introMC = document.getElementById("introMainContent");
 let introImage = document.getElementById("introIMG");
 
 function box1Expansion(){
     introMC.style.width = '90%';
-    introTC.style.margin = 'auto';
     return;
 }
-function timer(){
-    setTimeout(textAdditionContent, 400);
-}
 function textAdditionContent(){
-    introTC.append(introText);
-    introMC.appendChild(introTC);
+    introTextContainer.append(introText);
+    introMC.append(introTextContainer);
+    introMC.style.fontSize = '1.4vw';
+    introTextContainer.style.marginTop = 'auto';
+    introTextContainer.style.marginBottom = 'auto';
     return;
 }
 function textSubtractionOne(){
+    introText.remove();
     introMC.style.width = '12vw';
-    introTC.remove();
     return;
 }
 
 introMC.addEventListener('mouseout', textSubtractionOne);
 introMC.addEventListener('mouseover', box1Expansion);
-introMC.addEventListener('mouseover', timer);
+introMC.addEventListener('mouseover', textAdditionContent);
