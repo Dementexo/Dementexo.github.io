af84e669deffa4ba2f64f7d72d0e1a7a4bae81db
Dementexo <browndon962@gmail.com>
2022-06-28 18:59:37 -0500

    Fade-in text added for introText.

--- interactions.js
+++ interactions.js
@@ -2,20 +2,27 @@ let introTextContainer = document.createElement('iTC');
 let introText = document.createTextNode("Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.");
 let introMC = document.getElementById("introMainContent");
 let introImage = document.getElementById("introIMG");
+introTextContainer.style.opacity = 0;
 
 function box1Expansion(){
     introMC.style.width = '90%';
+    introMC.style.height = '12vw';
     return;
+}
+function fadeIn(){
+    introTextContainer.style.opacity = 1;
+    introTextContainer.style.transition = "2.5s opacity";
 }
 function textAdditionContent(){
     introTextContainer.append(introText);
     introMC.append(introTextContainer);
-    introMC.style.fontSize = '1.4vw';
+    introMC.style.fontSize = '1.2vw';
     introTextContainer.style.marginTop = 'auto';
-    introTextContainer.style.marginBottom = 'auto';
-    return;
+    introTextContainer.style.marginBottom = 'auto'; 
+    setTimeout(fadeIn, 800);
 }
 function textSubtractionOne(){
+    introTextContainer.style.opacity = 0;
     introText.remove();
     introMC.style.width = '12vw';
     return;
