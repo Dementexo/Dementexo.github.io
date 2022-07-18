274cfdb08ac53febdcd8d49cad917ec8a4cfa402
Dementexo <browndon962@gmail.com>
2022-07-18 18:08:07 -0500

    Fixed minor bug

--- interactions.js
+++ interactions.js
@@ -2,7 +2,6 @@ let introTextContainer = document.createElement('iTC');
 let introText = document.createTextNode("Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text. Sample text.");
 let introMC = document.getElementById("introMainContent");
 let introImage = document.getElementById("introIMG");
-introTextContainer.style.opacity = 0;
 
 function box1Expansion(){
     introMC.style.width = '90%';
@@ -10,16 +9,17 @@ function box1Expansion(){
     return;
 }
 function fadeIn(){
+    introTextContainer.style.transition = "2s opacity";
     introTextContainer.style.opacity = 1;
-    introTextContainer.style.transition = "2.5s opacity";
 }
 function textAdditionContent(){
     introTextContainer.append(introText);
+    introTextContainer.style.opacity = 0;
     introMC.append(introTextContainer);
     introMC.style.fontSize = '1.2vw';
     introTextContainer.style.marginTop = 'auto';
     introTextContainer.style.marginBottom = 'auto'; 
-    setTimeout(fadeIn, 800);
+    setTimeout(fadeIn, 300);
 }
 function textSubtractionOne(){
     introTextContainer.style.opacity = 0;
